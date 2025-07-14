fc6e3ea6e1c2aa07b28900df8d130f68ce54bd01
_ViniciusDeveloper <208984566+almaz-company@users.noreply.github.com>
2025-07-14 12:53:49 -0300

    fix

--- web/components/layout/header/links/index.ts
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-  { name: "Nossa Historia", href: "" },
-  { name: "Soluções", href: "" },
-  { name: "Financiamento", href: "" },
-  { name: "Blog", href: "" },
-  { name: "Contato", href: "" },
-];
